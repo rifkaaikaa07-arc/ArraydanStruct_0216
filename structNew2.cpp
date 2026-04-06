4ca5a4f173896ef270115543ea9a7566071d2dae
rifkaaikaa07-arc <rifkaaikaa07@gmail.com>
2026-04-06 19:46:39 +0700

    membuat struct dalam struct

---
+++ structNew2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+struct detailAlamat
+{
+    string desa;
+    string kota;
+};
+
+struct orang 
+{
+    string nama;
+    detailAlamat  alamat;
+    int umur;
+};
