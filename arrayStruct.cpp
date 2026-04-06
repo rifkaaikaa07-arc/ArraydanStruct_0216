6aace99abc8a10d571750c859e2f518180915d6e
rifkaaikaa07-arc <rifkaaikaa07@gmail.com>
2026-04-06 19:53:12 +0700

    menambahkan data yang lebih banyak

---
+++ arrayStruct.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct DetailAlamat
+{
+    string desa;
+    string kota;;
+};
+
+struct Orang
+{
+    string nama;
+    DetailAlamat alamat;
+    int umur;
+};
+
+int main() {
+    Orang mhs[3];
+    
+    for (int i = 0; i <= 2; i++)
+    {
+
+    // mengisi data
+    cout << "Mengisi Data" << endl;
+    cout << "Nama : ";
+    getline(cin ,  mhs[i].nama);
+    cout << "Alamat : ";
+    getline(cin , mhs[i].alamat.desa);
+    cout << "Alamat : ";
+    getline(cin , mhs[i].alamat.kota);
+    cout << "Umur : ";
+    cin >> mhs[i].umur;
+    cout << endl;
+
+    }
+}
